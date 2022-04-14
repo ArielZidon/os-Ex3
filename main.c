8874011b25e9d83d1f87665fcb8aba11042b2eec
ariel <arielz742@gmail.com>
2022-04-15 00:45:51 +0300

    first

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        int id = fork();
+        if (id)
+        {
+            printf("fork %d\n", id);
+            char *args[3] = {"./client", argv[1],  NULL};
+            execvp(args[0], args);
+        }
+        sleep(1);
+    }
+    return 0;
+}
